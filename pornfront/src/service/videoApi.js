88a384873e1c004b4f8f5ae998fb9562c1eab1fb
Pedro Verani <pedropverani@hotmail.com>
2024-07-13 23:11:16 -0300

    adjusting api

--- pornfront/src/service/videoApi.js
+++ pornfront/src/service/videoApi.js
@@ -1,5 +1,5 @@
 const fetchVideos = async (search) => {
-    const response = await fetch(`http://localhost:3000/?q=${search.replaceAll(' ', '+')}`);
+    const response = await fetch(`https://porn-engine-api.onrender.com/?q=${search.replaceAll(' ', '+')}`);
     const data = await response.json();
     return data;
 }
