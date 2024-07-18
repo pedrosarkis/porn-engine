9d4b7da31f9461d3d3e37a7c0add3afb9e8a1ac4
Pedro Verani <pedropverani@hotmail.com>
2024-07-18 05:27:21 -0300

    rollback vite config

--- pornfront/vite.config.js
+++ pornfront/vite.config.js
@@ -2,16 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import htmlPlugin from '@rollup/plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    htmlPlugin({
-      include: '**/*.html',
-      inject: true, 
-    }),
+    react()
   ],
   css: {
     postcss: {
